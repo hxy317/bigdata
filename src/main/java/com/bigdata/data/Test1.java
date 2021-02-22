c409919d1be1d5d33208617fa09a721ca22e98f6
huixinyu <xinyu.hui@colorv.com>
2021-02-22 17:13:12 +0800

    fix

--- src/main/java/com/bigdata/data/Test1.java
+++ src/main/java/com/bigdata/data/Test1.java
@@ -4,6 +4,6 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("1111");
-        System.out.println("3333");
+        System.out.println("2222");
     }
 }
