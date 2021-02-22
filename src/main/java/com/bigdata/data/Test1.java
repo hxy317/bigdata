48c67805b2b21360b7810b29a2b2d8eae304703e
huixinyu <xinyu.hui@colorv.com>
2021-02-22 18:36:48 +0800

    fix

--- src/main/java/com/bigdata/data/Test1.java
+++ src/main/java/com/bigdata/data/Test1.java
@@ -4,14 +4,5 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("1111");
-
-        System.out.println("2222");
-
-
-        System.out.println("4444");
-
-        System.out.println("5555");
-
-
     }
 }
